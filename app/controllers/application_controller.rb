dbaba871e26c30354f99d3a930f142be8cb05e36
Talha Waheed <talhawaheed92@gmail.com>
2017-06-16 00:56:53 +0500

    Redirect to Sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  #to redirect to the sign in page from any other page
+  before_action :authenticate_user!
 end
