5eafea68b63210a3213c4d16208ad3a2e9f431db
Talha Waheed <talhawaheed92@gmail.com>
2017-06-16 17:44:31 +0500

    Api Sign up Working

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::SessionsController < Api::ApiController
+
+# Api JSON Authentication with Devise
+# https://gist.github.com/jwo/1255275
+#
   
   respond_to :json
   skip_before_action :verify_authenticity_token
@@ -7,29 +11,33 @@ class Api::V1::SessionsController < Api::ApiController
 
 
   def create
-  	
+
   	resource = User.find_for_database_authentication(email: params[:user][:email])
-  		return invalid_login_attempt unless resource
+  	return invalid_login_attempt unless resource
+
   		if resource.valid_password?(params[:user][:password])
   			sign_in("user", resource)
   			render json: {status: "successful", user:{id: resource.id,
   				email: resource.email,
-  				# first_name: resource.first_name,
-  				# last_name: resource.last_name,
-  				# age: resource.age,
-  				# phone: resource.phone,
-  				# location: resource.location,
-  				# picture: resource.picture,
   				# authentication_token: resource.authentication_token,
   				created_at: resource.created_at,
   				updated_at: resource.updated_at  }}
-  				return
+  			return
   		end
   	invalid_login_attempt
   end
 
   def destroy
+  	#signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+  	signed_out = sign_out
+  	if signed_out
+  		render json: {status: "successful",msg: "You have signed out sucessfully"}
+  	else
+  		render json: {status: "failed",msg: "User Not signed in"}
+  	end
   end
+
+  protected
 
   def ensure_params_exist
   	return unless params[:user].blank?
