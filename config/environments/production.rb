de322b9e7e3a7b179915f0a93656b874527754e2
Talha Waheed <talhawaheed92@gmail.com>
2017-06-30 16:00:09 +0500

    email Debugging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,8 +89,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
+   :user_name            => "talhawaheed92@gmail.com", #ENV['gmail_username'],
+   :password             => "03424354470", #ENV['gmail_password'],
    :authentication       => "plain",
   :enable_starttls_auto => true
   }
