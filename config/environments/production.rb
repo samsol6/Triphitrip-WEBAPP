a5a2bebc38d8aefb3ed2b89ff7f240117955984d
Talha Waheed <talhawaheed92@gmail.com>
2017-06-30 17:09:21 +0500

    email Debugging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,23 +85,23 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  # SMTP settings for gmail
-  # config.action_mailer.smtp_settings = {
-  #  :address              => "smtp.gmail.com",
-  #  :port                 => 587,
-  #  :user_name            => "talhawaheed92@gmail.com", #ENV['gmail_username'],
-  #  :password             => "03424354470", #ENV['gmail_password'],
-  #  :authentication       => "plain",
-  # :enable_starttls_auto => true,
-  # }
+  SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => ENV['GMAIL_SMTP_USER'], #ENV['gmail_username'],
+   :password             => ENV['GMAIL_SMTP_PASSWORD'], #ENV['gmail_password'],
+   :authentication       => "plain",
+  :enable_starttls_auto => true,
+  }
 
-  ActionMailer::Base.smtp_settings = {
-  :port           => 587,
-  :address        => "smtp.mailgun.org",
-  # :domain         => ENV['domain'],
-  :user_name      => ENV['username'],
-  :password       => ENV['password'],
-  :authentication => :plain,
-}
+#   ActionMailer::Base.smtp_settings = {
+#   :port           => 587,
+#   :address        => "smtp.mailgun.org",
+#   # :domain         => ENV['domain'],
+#   :user_name      => ENV['username'],
+#   :password       => ENV['password'],
+#   :authentication => :plain,
+# }
 
 end
